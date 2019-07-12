3a933085463bee1caeae46f01eca653a1eb144af
jing <714195347@qq.com>
2019-07-12 17:49:29 +0800

    下载保存图片

--- pages/cover/cover.vue
+++ pages/cover/cover.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="black">
-		<image src="../../static/hot/1.jpg" class="cover" mode="widthFix"></image>
+		<image @longpress="operator" src="../../static/hot/1.jpg" class="cover" mode="widthFix"></image>
 		<!--动态绑定数据-->
 		<!-- <image
 			:src="cover"
@@ -22,13 +22,49 @@
 			this.cover = cover;
 			console.log(this.cover)
 			//通过api修改导航栏属性
-			uni.setNavigationBarColor({
-				frontColor:"#FFFFFF",
-				backgroundColor:"#000000"
-			})
+			// uni.setNavigationBarColor({
+			// 	frontColor:"#FFFFFF",
+			// 	backgroundColor:"#000000"
+			// })
 		},
 		methods: {
-			
+			operator(){
+				var _this = this;
+				uni.showActionSheet({
+					itemList:["下载图片","分享链接","其他"],
+					success: (res) => {
+						console.log(res);
+						if(res.tapIndex == 0){
+							console.log("亲,正在请求下载哦~~~~");
+							uni.showLoading({
+								title:"图片保存中~~~~"
+							})
+							
+							uni.downloadFile({
+								url:_this.cover,
+								success: (res) => {
+									console.log(result);
+									var tempFilePath = result.tempFilePath;
+									
+									uni.saveImageToPhotosAlbum({
+										filePath:tempFilePath,
+										success: () => {
+											uni.showToast({
+												title:"保存成功~~~",
+												duration:2000
+											})
+										},
+										complete: () => {
+											uni.hideLoading();
+										}
+									})	
+									
+								}
+							})
+						}
+					}
+				})
+			}
 		}
 	}
 </script>
